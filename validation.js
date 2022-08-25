2887a0ebaceca0215f5b8e083ca8c24e383d3c78
Razib2019 <hafizurrahmanrazib@gmail.com>
2022-08-25 07:42:52 +0600

    validation.js done

---
+++ validation.js
@@ -0,0 +1,28 @@
+// (typeof) check with: string, number, boolean,object
+/* const number = 23;
+if (typeof number === 'number') {
+    console.log('value is a number');
+}
+else {
+    console.log('value is not a number');
+} */
+
+
+// Arry Check: (Array.isArray())
+/* const numbers = [3, 12, 78, 90];
+const student = {
+    name: 'Rongila Rafsan',
+    job: 'Khawya Dawya Kora'
+}
+console.log(Array.isArray(numbers));
+console.log(Array.isArray(student));
+console.log(typeof student); */
+
+
+// Check isNaN: (isNan()) return true or false like array or boolean
+/* isNaN('2') = false
+isNaN(56) = false
+isNaN('ab') = true */
+
+
+// Check Undefined:
